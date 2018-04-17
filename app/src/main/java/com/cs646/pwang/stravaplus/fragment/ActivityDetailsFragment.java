04a20c4eab48e060cbc954441065970f352a6054
Pengyu Wang <bird0407@gmail.com>
2018-04-16 21:23:14 -0700

    map

--- app/src/main/java/com/cs646/pwang/stravaplus/fragment/ActivityDetailsFragment.java
+++ app/src/main/java/com/cs646/pwang/stravaplus/fragment/ActivityDetailsFragment.java
@@ -1,5 +1,6 @@
 package com.cs646.pwang.stravaplus.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,9 +9,19 @@ import android.view.ViewGroup;
 
 import com.cs646.pwang.stravaplus.R;
 import com.cs646.pwang.stravaplus.task.RetrieveActivityTask;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.android.PolyUtil;
 import com.sweetzpot.stravazpot.activity.model.Activity;
 
-public class ActivityDetailsFragment extends Fragment {
+import java.util.List;
+
+public class ActivityDetailsFragment extends Fragment implements OnMapReadyCallback {
+
+    private Activity currentActivity;
 
     public ActivityDetailsFragment() {
     }
@@ -25,11 +36,37 @@ public class ActivityDetailsFragment extends Fragment {
         RetrieveActivityTask task = new RetrieveActivityTask(this);
         task.execute(authToken, activityId);
 
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_activity_details, container, false);
     }
 
-    public void showActivityDetails(Activity activity) {
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        MapFragment mapFragment = (MapFragment) getActivity().getFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            getActivity().getFragmentManager().beginTransaction().remove(mapFragment).commit();
+        }
+    }
 
+    public void showActivityDetails(Activity activity) {
+        this.currentActivity = activity;
+
+        MapFragment mapFragment = (MapFragment) getActivity().getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        String encodedPolyline = currentActivity.getMap().getSummaryPolyline();
+
+        if (encodedPolyline == null) {
+            return;
+        }
+
+        List<LatLng> latlng = PolyUtil.decode(encodedPolyline);
+        googleMap.addPolyline(new PolylineOptions()
+                .color(Color.RED)
+        ).setPoints(latlng);
     }
 }
