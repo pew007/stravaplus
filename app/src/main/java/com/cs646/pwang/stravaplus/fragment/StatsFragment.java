211b7be65fd299914dc3048471a06e846fad4447
Pengyu Wang <bird0407@gmail.com>
2018-04-21 11:08:02 -0700

    stats fragment wip

--- app/src/main/java/com/cs646/pwang/stravaplus/fragment/StatsFragment.java
+++ app/src/main/java/com/cs646/pwang/stravaplus/fragment/StatsFragment.java
@@ -1,30 +1,52 @@
 package com.cs646.pwang.stravaplus.fragment;
 
-
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.cs646.pwang.stravaplus.R;
+import com.github.mikephil.charting.charts.LineChart;
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class StatsFragment extends Fragment {
+public class StatsFragment extends ListFragment implements AdapterView.OnItemClickListener {
 
+    private String[] options;
 
     public StatsFragment() {
-        // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stats, container, false);
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        options = getActivity().getResources().getStringArray(R.array.stats_options);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                options
+        );
+
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.i("pw", options[i]);
+    }
 }
